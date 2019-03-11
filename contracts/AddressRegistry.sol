4d886d14da43058b4d33d19f71a76f95d62ee220
Sowmayjain <champ.sowmay@gmail.com>
2019-03-12 04:39:59 +0530

    contract merged in address registry.

--- contracts/AddressRegistry.sol
+++ contracts/AddressRegistry.sol
@@ -3,7 +3,12 @@ pragma solidity ^0.5.0;
 
 contract AddressRegistry {
     event AddressSet(string name, address addr);
+    event DefaultLogicSet(address logicAddr);
+    event LogicSet(address logicAddr, bool isLogic);
+
     mapping(bytes32 => address) registry;
+    mapping(address => bool) public defaultLogicProxies;
+    mapping(address => bool) public logicProxies;
 
     constructor() public {
         registry[keccak256(abi.encodePacked("admin"))] = msg.sender;
@@ -32,16 +37,6 @@ contract AddressRegistry {
         _;
     }
 
-}
-
-
-contract LogicRegistry is AddressRegistry {
-    event DefaultLogicSet(address logicAddr);
-    event LogicSet(address logicAddr, bool isLogic);
-
-    mapping(address => bool) public defaultLogicProxies;
-    mapping(address => bool) public logicProxies;
-
     function getLogic(address logicAddr) public view returns (bool) {
         if (defaultLogicProxies[logicAddr]) {
             return true;
@@ -52,16 +47,16 @@ contract LogicRegistry is AddressRegistry {
         }
     }
 
-    function setLogic(address logicAddr, bool isLogic) public isAdmin {
-        require(msg.sender == getAddress("admin"), "Permission Denied");
-        logicProxies[logicAddr] = true;
-        emit LogicSet(logicAddr, isLogic);
-    }
-
     function setDefaultLogic(address logicAddr) public isAdmin {
         require(msg.sender == getAddress("admin"), "Permission Denied");
         defaultLogicProxies[logicAddr] = true;
         emit DefaultLogicSet(logicAddr);
+    }
+
+    function setLogic(address logicAddr, bool isLogic) public isAdmin {
+        require(msg.sender == getAddress("admin"), "Permission Denied");
+        logicProxies[logicAddr] = true;
+        emit LogicSet(logicAddr, isLogic);
     }
 
 }
