aa7d18978d311598b97f0f3c7a5c6dd060fa927c
Sowmayjain <champ.sowmay@gmail.com>
2019-06-18 23:50:02 +0530

    removed unused interface functions.

--- contracts/bridges/MakerCompound.sol
+++ contracts/bridges/MakerCompound.sol
@@ -10,24 +10,9 @@ interface UserWalletInterface {
 
 interface CTokenInterface {
     function mint(uint mintAmount) external returns (uint); // For ERC20
-    function repayBorrow(uint repayAmount) external returns (uint); // For ERC20
-    function repayBorrowBehalf(address borrower, uint repayAmount) external returns (uint); // For ERC20
-    function borrowBalanceCurrent(address account) external returns (uint);
     function redeem(uint redeemTokens) external returns (uint);
     function redeemUnderlying(uint redeemAmount) external returns (uint);
-    function borrow(uint borrowAmount) external returns (uint);
-    function liquidateBorrow(address borrower, uint repayAmount, address cTokenCollateral) external returns (uint);
-    function liquidateBorrow(address borrower, address cTokenCollateral) external payable;
     function exchangeRateCurrent() external returns (uint);
-    function getCash() external view returns (uint);
-    function totalBorrowsCurrent() external returns (uint);
-    function borrowRatePerBlock() external view returns (uint);
-    function supplyRatePerBlock() external view returns (uint);
-    function totalReserves() external view returns (uint);
-    function reserveFactorMantissa() external view returns (uint);
-
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address owner) external view returns (uint256 balance);
     function allowance(address, address) external view returns (uint);
     function approve(address, uint) external;
     function transfer(address, uint) external returns (bool);
@@ -36,7 +21,6 @@ interface CTokenInterface {
 
 interface ERC20Interface {
     function allowance(address, address) external view returns (uint);
-    function balanceOf(address) external view returns (uint);
     function approve(address, uint) external;
     function transfer(address, uint) external returns (bool);
     function transferFrom(address, address, uint) external returns (bool);
