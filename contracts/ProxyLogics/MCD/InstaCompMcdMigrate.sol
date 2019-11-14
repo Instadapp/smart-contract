5fe09f80a81621c1579c2f5d5b52edc87ae5fcba
Thrilok Kumar <thrilok2000@gmail.com>
2019-11-15 04:49:41 +0530

    changes according to new mcd changes 0.2.17

--- contracts/ProxyLogics/MCD/InstaCompMcdMigrate.sol
+++ contracts/ProxyLogics/MCD/InstaCompMcdMigrate.sol
@@ -91,34 +91,23 @@ interface OtcInterface {
     ) external;
 }
 
-interface ManagerLike {
-    function cdpCan(address, uint, address) external view returns (uint);
-    function ilks(uint) external view returns (bytes32);
-    function owns(uint) external view returns (address);
-    function urns(uint) external view returns (address);
-    function vat() external view returns (address);
-    function open(bytes32) external returns (uint);
-    function give(uint, address) external;
-    function cdpAllow(uint, address, uint) external;
-    function urnAllow(address, uint) external;
-    function frob(uint, int, int) external;
-    function frob(
-        uint,
-        address,
-        int,
-        int
-    ) external;
-    function flux(uint, address, uint) external;
-    function move(uint, address, uint) external;
-    function exit(
-        address,
-        uint,
-        address,
-        uint
-    ) external;
-    function quit(uint, address) external;
-    function enter(address, uint) external;
-    function shift(uint, uint) external;
+contract ManagerLike {
+    function cdpCan(address, uint, address) public view returns (uint);
+    function ilks(uint) public view returns (bytes32);
+    function owns(uint) public view returns (address);
+    function urns(uint) public view returns (address);
+    function vat() public view returns (address);
+    function open(bytes32, address) public returns (uint);
+    function give(uint, address) public;
+    function cdpAllow(uint, address, uint) public;
+    function urnAllow(address, uint) public;
+    function frob(uint, int, int) public;
+    function flux(uint, address, uint) public;
+    function move(uint, address, uint) public;
+    function exit(address, uint, address, uint) public;
+    function quit(uint, address) public;
+    function enter(address, uint) public;
+    function shift(uint, uint) public;
 }
 
 interface GemLike {
@@ -419,7 +408,7 @@ contract CompoundResolver is LiquidityResolver {
 contract McdResolver is CompoundResolver {
     function open() internal returns (uint cdp) {
         bytes32 ilk = 0x4554482d41000000000000000000000000000000000000000000000000000000;
-        cdp = ManagerLike(getManagerAddress()).open(ilk);
+        cdp = ManagerLike(getManagerAddress()).open(ilk, address(this));
     }
 
     function lockETH(
