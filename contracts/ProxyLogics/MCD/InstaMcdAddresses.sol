573936a915414985f671d1539d55b75860c0b7ef
Thrilok Kumar <thrilok2000@gmail.com>
2019-11-16 03:29:55 +0530

    Added mcd addresses contract

---
+++ contracts/ProxyLogics/MCD/InstaMcdAddresses.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.5.8;
+
+
+contract McdAddress {
+    address public manager = 0x5ef30b9986345249bc32d8928B7ee64DE9435E39;
+    address public dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public daiJoin = 0x9759A6Ac90977b93B58547b4A71c78317f391A28;
+    address public vat = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+    address public jug = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
+    address public cat = 0x78F2c2AF65126834c51822F56Be0d7469D7A523E;
+    address public gov = 0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2;
+    address public adm = 0x9eF05f7F6deB616fd37aC3c959a2dDD25A54E4F5;
+    address public vow = 0xA950524441892A31ebddF91d3cEEFa04Bf454466;
+    address public spot = 0x65C79fcB50Ca1594B025960e539eD7A9a6D434A3;
+    address public pot = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
+    address public esm = 0x0581A0AbE32AAe9B5f0f68deFab77C6759100085;
+    address public mcdFlap = 0xdfE0fb1bE2a52CDBf8FB962D5701d7fd0902db9f;
+    address public mcdFlop = 0xBE00FE8Dfd9C079f1E5F5ad7AE9a3Ad2c571FCAC;
+    address public mcdDeploy = 0xbaa65281c2FA2baAcb2cb550BA051525A480D3F4;
+    address public mcdEnd = 0xaB14d3CE3F733CACB76eC2AbE7d2fcb00c99F3d5;
+    address public proxyActions = 0x82ecD135Dce65Fbc6DbdD0e4237E0AF93FFD5038;
+    address public proxyActionsEnd = 0x069B2fb501b6F16D1F5fE245B16F6993808f1008;
+    address public proxyActionsDsr = 0x07ee93aEEa0a36FfF2A9B95dd22Bd6049EE54f26;
+    address public getCdps = 0x36a724Bd100c39f0Ea4D3A20F7097eE01A8Ff573;
+    address public saiTub = 0x448a5065aeBB8E423F0896E6c5D525C040f59af3;
+    address public weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public bat = 0x0D8775F648430679A709E98d2b0Cb6250d2887EF;
+    address public sai = 0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359;
+    address public ethAJoin = 0x2F0b23f53734252Bda2277357e97e1517d6B042A;
+    address public ethAFlip = 0xd8a04F5412223F513DC55F839574430f5EC15531;
+    address public batAJoin = 0x3D0B1912B66114d4096F48A8CEe3A56C231772cA;
+    address public batAFlip = 0xaA745404d55f88C108A28c86abE7b5A1E7817c07;
+    address public ethPip = 0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763;
+    address public batAPip = 0xB4eb54AF9Cc7882DF0121d26c5b97E802915ABe6;
+    address public saiJoin = 0xad37fd42185Ba63009177058208dd1be4b136e6b;
+    address public saiFlip = 0x5432b2f3c0DFf95AA191C45E5cbd539E2820aE72;
+    address public saiPip = 0x54003DBf6ae6CBa6DDaE571CcdC34d834b44Ab1e;
+    address payable public migration = 0xc73e0383F3Aff3215E6f04B0331D58CeCf0Ab849;
+    // address public mcdPause = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
+}
+
+/**
+interface InstaMcdAddress {
+    function manager() external returns (address);
+    function dai() external returns (address);
+    function daiJoin() external returns (address);
+    function vat() external returns (address);
+    function jug() external returns (address);
+    function cat() external returns (address);
+    function gov() external returns (address);
+    function adm() external returns (address);
+    function vow() external returns (address);
+    function spot() external returns (address);
+    function pot() external returns (address);
+    function esm() external returns (address);
+    function mcdFlap() external returns (address);
+    function mcdFlop() external returns (address);
+    function mcdDeploy() external returns (address);
+    function mcdEnd() external returns (address);
+    function proxyActions() external returns (address);
+    function proxyActionsEnd() external returns (address);
+    function proxyActionsDsr() external returns (address);
+    function getCdps() external returns (address);
+    function saiTub() external returns (address);
+    function weth() external returns (address);
+    function bat() external returns (address);
+    function sai() external returns (address);
+    function ethAJoin() external returns (address);
+    function ethAFlip() external returns (address);
+    function batAJoin() external returns (address);
+    function batAFlip() external returns (address);
+    function ethPip() external returns (address);
+    function batAPip() external returns (address);
+    function saiJoin() external returns (address);
+    function saiFlip() external returns (address);
+    function saiPip() external returns (address);
+    function migration() external returns (address payable);
+}
+ */
